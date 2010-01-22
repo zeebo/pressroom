a4948ea8f9a1d10c2db32bc7995188343d680058
zeebo <leterip@gmail.com>
2010-01-22 09:31:16 -0500

    fixed template dir and added empty base template

--- settings.py
+++ settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'pressroom.urls'
 
 TEMPLATE_DIRS = (
-    '/Users/zeebo/Django/saaa/templates/',
+    os.path.join(CURRENT_PATH, 'templates/'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
