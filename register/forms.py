1e08fcd9da83a14cf13413bc6e14d90af4acad68
Zeebo <leterip@gmail.com>
2010-01-23 15:21:35 -0500

    added validation to reg form to make sure email isnt in system already

--- register/forms.py
+++ register/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class RegistrationForm(forms.Form):
   email = forms.EmailField(label="Your E-Mail")
-  password = forms.CharField(widget=forms.PasswordInput, label="Your Password")
   first_name = forms.CharField(max_length=20, label="Your first name")
   last_name = forms.CharField(max_length=20, label="Your last name")
+  password = forms.CharField(widget=forms.PasswordInput, label="Your Password")  
+  
+  def clean_email(self):
+    data = self.cleaned_data['email']
+    if data:
+      if User.objects.filter(username=data).count() > 0:
+        raise forms.ValidationError("Email already registered")
+    
+    return data
