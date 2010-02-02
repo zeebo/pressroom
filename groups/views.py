8ee774b5c7331bff48fc401209231c8d7ad71a30
zeebo <leterip@gmail.com>
2010-02-02 13:52:22 -0500

    made group views use reverse

--- groups/views.py
+++ groups/views.py
@@ -2,21 +2,21 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User, Group
 from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 @login_required
 def add_group(request, group_name):
   group = get_object_or_404(Group, name=group_name)
   request.user.groups.add(group)
   request.user.message_set.create(message="Added to %s email list." % group_name)
-  return HttpResponseRedirect('/companies/%s/' % group_name) #Fix to be generic
-  
+  return HttpResponseRedirect(reverse(view_group, kwargs={'group_name':group_name}))
+
 @login_required
 def del_group(request, group_name):
   group = get_object_or_404(Group, name=group_name)
   request.user.groups.remove(group)
   request.user.message_set.create(message="Removed from %s email list." % group_name)
-  return HttpResponseRedirect('/companies/%s/' % group_name) #Fix to be generic
-
+  return HttpResponseRedirect(reverse(view_group, kwargs={'group_name':group_name}))
 
 #Fix to get message set for the group
 def view_group(request, group_name):
