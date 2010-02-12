51b8e5eed8e19fe335ba5fc8b8d018d5bd371a94
zeebo <leterip@gmail.com>
2010-02-12 16:32:42 -0500

    fixed reverse function in group views

--- groups/views.py
+++ groups/views.py
@@ -10,14 +10,14 @@ def add_group(request, group_name):
   group = get_object_or_404(Group, name=group_name)
   request.user.groups.add(group)
   request.user.message_set.create(message="Added to %s email list." % group_name)
-  return HttpResponseRedirect(reverse('group_detail', kwargs={'group_name':group_name}))
+  return HttpResponseRedirect(reverse(group_detail, kwargs={'group_name':group_name}))
 
 @login_required
 def del_group(request, group_name):
   group = get_object_or_404(Group, name=group_name)
   request.user.groups.remove(group)
   request.user.message_set.create(message="Removed from %s email list." % group_name)
-  return HttpResponseRedirect(reverse('group_detail', kwargs={'group_name':group_name}))
+  return HttpResponseRedirect(reverse(group_detail, kwargs={'group_name':group_name}))
 
 def group_detail(request, group_name):
   group_set = Group.objects.filter(name__exact=group_name)
