53d69ddfb56760e78265de08559b37fe71af7011
zeebo <leterip@gmail.com>
2010-02-13 05:13:52 -0500

    updated post model to make correct slug titles

--- posts/models.py
+++ posts/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import Group
 from django.contrib import admin
 from django.utils.safestring import mark_safe
+from django.template.defaultfilters import slugify
 import datetime
 
 # Create your models here.
 class Post(models.Model):
   title = models.CharField(max_length=100)
   body = models.TextField()
-  slug = models.SlugField(max_length=50)
+  slug = models.SlugField(max_length=50, editable=False)
   company = models.ForeignKey(Group)
   time_released = models.DateTimeField()
   
@@ -17,8 +18,12 @@ class Post(models.Model):
     
   def display_body(self):
     return mark_safe(self.body)
+    
+  def save(self):
+    if not self.id:
+      self.slug = slugify(self.title)
+    super(self.__class__, self).save()
 
 
 class PostAdmin(admin.ModelAdmin):
-  prepopulated_fields = {"slug": ("title", )}
   list_display = ('slug', 'company', 'time_released', 'was_published_today')
