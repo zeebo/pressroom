289b5b027401818c32b21e460853182b98c1afff
zeebo <leterip@gmail.com>
2010-02-26 14:36:30 -0500

    added user_admin to installed apps

--- settings.py
+++ settings.py
@@ -87,6 +87,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'pressroom.register',
     'pressroom.posts',
+#    'pressroom.companies',
+    'pressroom.user_admin',
 )
 
 AUTHENTICATION_BACKENDS = (
