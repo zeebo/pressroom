f953bdfe6ad479af74dedb7da45606bd8f4c88b5
zeebo <leterip@gmail.com>
2010-03-01 15:19:41 -0500

    added comments to company model

--- companies/models.py
+++ companies/models.py
@@ -16,9 +16,9 @@ class Company(models.Model):
   class Meta:
     verbose_name_plural = 'Companies'
   
+  #Dont allow the model to be deleted.
+  #Use the inactive field instead.
   def delete(self, *args, **kwargs):
-    import logging
-    logging.error('what the fuck')
     self.active = False
     self.save()
     
@@ -28,21 +28,27 @@ class CompanyAdminForm(forms.ModelForm):
     model = Company
     fields = ('full_name', 'short_name', 'stock_ticker', 'website', 'blurb')
   
+  #Add a field to abstract the group model from user interaction
   short_name = forms.CharField(max_length=100, required=True)
   
   def __init__(self, *args, **kwargs):
+    #Populate the short_name field if in edit mode (instance is defined)
     if 'instance' in kwargs:
       self.declared_fields['short_name'].initial = kwargs['instance'].group.name
       
     forms.ModelForm.__init__(self, *args, **kwargs)
   
   def clean_short_name(self):
+    
+    #Check to see if the group is defined on the instance
+    #If so we are in edit mode
     instance_group = None
     try:
       instance_group = self.instance.group
     except Group.DoesNotExist:
       pass
     
+    #If we're in edit mode, dont allow short_name to be changed
     if 'short_name' in self.changed_data and instance_group:
       raise forms.ValidationError('Can\'t change the short name for a company')
     else:
@@ -62,6 +68,7 @@ class CompanyAdmin(admin.ModelAdmin):
     queryset.update(active=True)
   make_active.short_description = "Mark selected companies as active"
   
+  #Remove delete from the list of actions to force active/inactive.
   def get_actions(self, request):
     actions = super(CompanyAdmin, self).get_actions(request)
     del actions['delete_selected']
